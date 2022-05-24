3872be5de51976bde8cd42b8c8fe8aa04aa5887d
richard <richardkenyon1991@hotmail.co.uk>
2022-05-24 12:46:47 +0100

    first pass of stock overview selection functions. Can be improved, but will suffice for now

--- server/stockManagement.py
+++ server/stockManagement.py
@@ -6,9 +6,9 @@ import datetime
 from auth import login_required
 from db import getDbSession, Settings
 from qrCodeFunctions import convertDpiAndMmToPx, generateItemIdQrCodeSheets
-from dbSchema import StockItem, ProductType
+from dbSchema import StockItem, ProductType, AssignedStock
 
-from sqlalchemy import select, delete
+from sqlalchemy import select, delete, func, or_
 
 bp = Blueprint('stockManagement', __name__)
 
@@ -130,6 +130,245 @@ def getStock():
             "productBarcode": row[7]
         })
     return make_response(jsonify(stockList), 200)
+
+
+@bp.route('/getStockOverview')
+@login_required
+def getStockOverview():
+    overviewType = request.args.get('overviewType', default='totalStock')
+
+    if overviewType == "totalStock":
+        stockList = getStockOverviewTotals()
+    elif overviewType == "availableStock":
+        stockList = getAvailableStockTotals()
+    elif overviewType == "nearExpiry":
+        stockList = getStockNearExpiry()
+    elif overviewType == "expired":
+        stockList = getExpiredStock()
+
+
+    return make_response(jsonify(stockList), 200)
+
+
+def getStockOverviewTotals():
+    session = getDbSession()
+    if "searchTerm" in request.args:
+        searchTerm = "%" + request.args.get("searchTerm") + "%"
+    else:
+        searchTerm = None
+
+    query = session.query(
+        ProductType.id,
+        ProductType.productName,
+        ProductType.tracksAllItemsOfProductType,
+        ProductType.productDescriptor1,
+        ProductType.productDescriptor2,
+        ProductType.productDescriptor3,
+        ProductType.addedTimestamp,
+        func.sum(StockItem.quantityRemaining),
+        ProductType.barcode) \
+        .filter(StockItem.productType == ProductType.id) \
+        .group_by(StockItem.productType) \
+        .order_by(ProductType.productName.asc())
+
+    if searchTerm:
+        query = query.where(
+            or_(
+                ProductType.productName.ilike(searchTerm),
+                ProductType.productDescriptor1.ilike(searchTerm),
+                ProductType.productDescriptor2.ilike(searchTerm),
+                ProductType.productDescriptor3.ilike(searchTerm),
+                ProductType.barcode.ilike(searchTerm)
+            )
+        )
+
+    # ... run it and convert to a dict....
+    result = query.all()
+    productList = [
+        {
+            "productId": row[0],
+            "productName": row[1],
+            "isBulk": row[2],
+            "descriptor1": row[3],
+            "descriptor2": row[4],
+            "descriptor3": row[5],
+            "addedTimestamp": row[6],
+            "stockAmount": row[7]
+        }
+        for row in result
+    ]
+
+    return productList
+
+
+def getAvailableStockTotals():
+    session = getDbSession()
+    if "searchTerm" in request.args:
+        searchTerm = "%" + request.args.get("searchTerm") + "%"
+    else:
+        searchTerm = "%"
+
+    # get all the other data first
+    productTypesQueryResult = session.query(ProductType)\
+        .order_by(ProductType.productName.asc())\
+        .filter(
+            or_(
+                ProductType.productName.ilike(searchTerm),
+                ProductType.productDescriptor1.ilike(searchTerm),
+                ProductType.productDescriptor2.ilike(searchTerm),
+                ProductType.productDescriptor3.ilike(searchTerm),
+                ProductType.barcode.ilike(searchTerm)
+            )
+        )\
+        .all()
+
+    productList = [
+        {
+            "productId": row.id,
+            "productName": row.productName,
+            "isBulk": row.tracksAllItemsOfProductType,
+            "descriptor1": row.productDescriptor1,
+            "descriptor2": row.productDescriptor2,
+            "descriptor3": row.productDescriptor3,
+            "addedTimestamp": row.addedTimestamp,
+        }
+        for row in productTypesQueryResult
+    ]
+
+    # then get total stock and make a nice dict
+    # TODO: see if the DB can do this.
+    stockQueryResult = session.query(StockItem.productType, func.sum(StockItem.quantityRemaining))\
+        .group_by(StockItem.productType)\
+        .all()
+    stockDict = {row[0]: row[1] for row in stockQueryResult}
+
+    # then get assigned stock
+    assignedStockQueryResult = session.query(AssignedStock.productId, func.sum(AssignedStock.quantity))\
+        .group_by(AssignedStock.productId)\
+        .all()
+
+    # then work out how much stock is unassigned
+    for row in assignedStockQueryResult:
+        productId = row[0]
+        assignedQty = row[1]
+        stockDict[productId] = stockDict[productId] - assignedQty
+
+    # then update the product list
+    for product in productList:
+        if product['productId'] in stockDict:
+            product['stockAvailable'] = stockDict[product['productId']]
+        else:
+            product['stockAvailable'] = None
+
+    return productList
+
+
+def getStockNearExpiry():
+    session = getDbSession()
+    if "searchTerm" in request.args:
+        searchTerm = "%" + request.args.get("searchTerm") + "%"
+    else:
+        searchTerm = "%"
+
+    # get how close to expiry an item of stock needs to be to be counted
+    dayCountLimit = request.args.get("dayCountLimit", type=int, default=10)
+    maxExpiryDate = datetime.date.today() + datetime.timedelta(days=dayCountLimit)
+
+    query = session.query(
+        ProductType.id,
+        ProductType.productName,
+        ProductType.tracksAllItemsOfProductType,
+        ProductType.productDescriptor1,
+        ProductType.productDescriptor2,
+        ProductType.productDescriptor3,
+        ProductType.addedTimestamp,
+        func.sum(StockItem.quantityRemaining),
+        ProductType.barcode) \
+        .filter(StockItem.productType == ProductType.id) \
+        .group_by(StockItem.productType) \
+        .order_by(ProductType.productName.asc()) \
+        .filter(StockItem.expiryDate <= maxExpiryDate)
+
+    if searchTerm:
+        query = query.where(
+            or_(
+                ProductType.productName.ilike(searchTerm),
+                ProductType.productDescriptor1.ilike(searchTerm),
+                ProductType.productDescriptor2.ilike(searchTerm),
+                ProductType.productDescriptor3.ilike(searchTerm),
+                ProductType.barcode.ilike(searchTerm)
+            )
+        )
+
+    # ... run it and convert to a dict....
+    result = query.all()
+    productList = [
+        {
+            "productId": row[0],
+            "productName": row[1],
+            "isBulk": row[2],
+            "descriptor1": row[3],
+            "descriptor2": row[4],
+            "descriptor3": row[5],
+            "addedTimestamp": row[6],
+            "stockAmount": row[7]
+        }
+        for row in result
+    ]
+
+    return productList
+
+
+def getExpiredStock():
+    session = getDbSession()
+    if "searchTerm" in request.args:
+        searchTerm = "%" + request.args.get("searchTerm") + "%"
+    else:
+        searchTerm = "%"
+
+    query = session.query(
+        ProductType.id,
+        ProductType.productName,
+        ProductType.tracksAllItemsOfProductType,
+        ProductType.productDescriptor1,
+        ProductType.productDescriptor2,
+        ProductType.productDescriptor3,
+        ProductType.addedTimestamp,
+        func.sum(StockItem.quantityRemaining),
+        ProductType.barcode) \
+        .filter(StockItem.productType == ProductType.id) \
+        .group_by(StockItem.productType) \
+        .order_by(ProductType.productName.asc()) \
+        .filter(StockItem.expiryDate <= datetime.date.today())
+
+    if searchTerm:
+        query = query.where(
+            or_(
+                ProductType.productName.ilike(searchTerm),
+                ProductType.productDescriptor1.ilike(searchTerm),
+                ProductType.productDescriptor2.ilike(searchTerm),
+                ProductType.productDescriptor3.ilike(searchTerm),
+                ProductType.barcode.ilike(searchTerm)
+            )
+        )
+
+    # ... run it and convert to a dict....
+    result = query.all()
+    productList = [
+        {
+            "productId": row[0],
+            "productName": row[1],
+            "isBulk": row[2],
+            "descriptor1": row[3],
+            "descriptor2": row[4],
+            "descriptor3": row[5],
+            "addedTimestamp": row[6],
+            "stockAmount": row[7]
+        }
+        for row in result
+    ]
+
+    return productList
 
 
 @bp.route('/editStockItem', methods=("POST",))
